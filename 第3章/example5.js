2c018324e2b57cb9eb68e6da854fab0d4f8cd321
LBM <3095088766@qq.com>
2024-02-12 20:01:08 +0800

    学到了数据类型

---
+++ 第3章/example5.js
@@ -0,0 +1,4 @@
+var name ='matt';
+console.log(window.name);//'matt'
+let age=26;
+console.log(window.age);//undefined
---
+++ 第3章/example6.js
@@ -0,0 +1,10 @@
+for (let i=0;i<5;++i){
+    setTimeout(() => {
+        console.log(i)
+    }, 0);
+}
+for (var i=5;i<10;++i){
+    setTimeout(() => {
+        console.log(i)
+    }, 0);
+}
