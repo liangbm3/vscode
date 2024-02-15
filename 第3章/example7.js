0af0567e54a09a98476c9ad70482e6e7f21439a9
LBM <3095088766@qq.com>
2024-02-15 09:12:12 +0800

    更新

---
+++ 第3章/example7.js
@@ -0,0 +1,6 @@
+let string1='first line\nsecond line';
+let string2=`first line
+second line`;
+console.log(string1);
+console.log(string2);
+console.log(string1==string2);
---
+++ 第3章/example8.js
@@ -0,0 +1,5 @@
+let value=5;
+let exponent='second';
+let string=
+    `${ value } to the ${exponent} power is ${value*value}`;
+console.log(string);
